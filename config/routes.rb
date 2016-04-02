78c7f042b9951f842b481863536dd5ba5e52fb5c
mardiniii <sezama03@hotmail.com>
2016-04-02 11:51:40 -0500

    Add events controller with initial views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'events/new'
+
+  get 'events/edit'
+
+  get 'events/index'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
