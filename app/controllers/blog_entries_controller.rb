eecd36b8585c3b60f0404e2d3f5c5985bb3a96ef
mardiniii <sezama03@hotmail.com>
2016-04-05 19:14:37 -0500

    Add index view for blog entries

--- app/controllers/blog_entries_controller.rb
+++ app/controllers/blog_entries_controller.rb
@@ -4,4 +4,24 @@ class BlogEntriesController < ApplicationController
   def new
     @blog_entry = BlogEntry.new
   end
+
+  def create
+    @blog_entry = BlogEntry.new(blog_entry_params)
+    if @blog_entry.save
+      flash[:notice] = "La entrada de blog ha sido guardada con éxito"
+      redirect_to blog_entries_path
+    else
+      flash[:alert] = "Ha ocurrido un problema y la entrada de blog no ha sido almacenada"
+      render :action => 'new'
+    end
+  end
+
+  def index
+    @blog_entries = BlogEntry.all
+  end
+
+  private
+    def blog_entry_params
+      params.require(:blog_entry).permit(:title, :author, :image, :content)
+    end
 end
