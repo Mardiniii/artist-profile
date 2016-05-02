55f9599baadbc4f1afe165ae0f033d33b0c15c44
mardiniii <sezama03@hotmail.com>
2016-05-02 18:43:22 -0500

    Add initial actions for music tracks

--- app/controllers/music_tracks_controller.rb
+++ app/controllers/music_tracks_controller.rb
@@ -1,10 +1,14 @@
 class MusicTracksController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @music_track = MusicTrack.new
   end
 
   def edit
   end
 
   def index
+    @music_tracks = MusicTrack.all
   end
 end
