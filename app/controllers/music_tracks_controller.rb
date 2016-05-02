81af9e137150282ede00b68f7d2801f4a95fd041
mardiniii <sezama03@hotmail.com>
2016-05-02 18:58:50 -0500

    Add functionality to create, update and destroy music tracks

--- app/controllers/music_tracks_controller.rb
+++ app/controllers/music_tracks_controller.rb
@@ -5,10 +5,44 @@ class MusicTracksController < ApplicationController
     @music_track = MusicTrack.new
   end
 
+  def create
+    @music_track = MusicTrack.new(music_track_params)
+    if @music_track.save
+      flash[:notice] = "La canción ha sido guardada con éxito"
+      redirect_to music_tracks_path
+    else
+      flash[:alert] = "Ha ocurrido un problema y la canción no ha sido almacenada"
+      render :action => 'new'
+    end
+  end
+
   def edit
+    @music_track = MusicTrack.find(params[:id])
+  end
+
+  def update
+    @music_track = MusicTrack.find(params[:id])
+    if @music_track.update(music_track_params)
+      flash[:notice] = "La canción ha sido editada con éxito"
+      redirect_to music_tracks_path
+    else
+      flash[:alert] = 'Ha ocurrido un problema y la canción no ha sido editada'
+      render 'edit'
+    end
   end
 
   def index
     @music_tracks = MusicTrack.all
   end
+
+  def destroy
+    @music_track = MusicTrack.find(params[:id])
+    @music_track.destroy
+    redirect_to music_tracks_path
+  end
+
+  private
+    def music_track_params
+      params.require(:music_track).permit(:name, :author, :image)
+    end
 end
