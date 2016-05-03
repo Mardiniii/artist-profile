6acf115a7e5039f2c54984cb4b9786a73ab48e10
mardiniii <sezama03@hotmail.com>
2016-05-03 15:39:43 -0500

    Run migration with paperclip for track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,10 +2,17 @@
 #
 # Table name: tracks
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  audio_file_name    :string
+#  audio_content_type :string
+#  audio_file_size    :integer
+#  audio_updated_at   :datetime
 #
 
 class Track < ActiveRecord::Base
+  has_attached_file :audio
+  validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+  validates_attachment_presence :logo
 end
--- test/models/track_test.rb
+++ test/models/track_test.rb
@@ -2,9 +2,13 @@
 #
 # Table name: tracks
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  audio_file_name    :string
+#  audio_content_type :string
+#  audio_file_size    :integer
+#  audio_updated_at   :datetime
 #
 
 require 'test_helper'
