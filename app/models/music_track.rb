d9b4d8d228ad33722aa24adecc2fba818b3a7ffb
mardiniii <sezama03@hotmail.com>
2016-05-03 15:42:23 -0500

    Add associations inside model music track for nested attributes

--- app/models/music_track.rb
+++ app/models/music_track.rb
@@ -14,8 +14,13 @@
 #
 
 class MusicTrack < ActiveRecord::Base
+  has_one :track, dependent: :destroy
+  accepts_nested_attributes_for :tracks, :limit => 1, :reject_if => :all_blank, allow_destroy: true
+
   has_attached_file :image, styles: { medium: "300x150#", thumb: "100X100#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   validates :name, :author, :content, :image, presence: { message: "Todos los campos son obligatorios"}
+
+  validates_associated :tracks
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -12,6 +12,8 @@
 #
 
 class Track < ActiveRecord::Base
+  belongs_to :music_track
+
   has_attached_file :audio
   validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
   validates_attachment_presence :logo
