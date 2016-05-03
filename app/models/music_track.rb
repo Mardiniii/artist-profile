90d78073b63f73250a9150b999770ca5286ed2e0
mardiniii <sezama03@hotmail.com>
2016-05-03 16:39:29 -0500

    Fix error to create tracks into music track model

--- app/models/music_track.rb
+++ app/models/music_track.rb
@@ -20,7 +20,7 @@ class MusicTrack < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x150#", thumb: "100X100#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-  validates :name, :author, :content, :image, presence: { message: "Todos los campos son obligatorios"}
+  validates :name, :author, :image, presence: { message: "Todos los campos son obligatorios"}
 
   validates_associated :tracks
 end
