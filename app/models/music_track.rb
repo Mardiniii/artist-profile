511ca516cc3af97287075ab518293d6fbb3cf1d4
mardiniii <sezama03@hotmail.com>
2016-05-23 17:04:45 -0500

    Add new size to music track model

--- app/models/music_track.rb
+++ app/models/music_track.rb
@@ -17,7 +17,7 @@ class MusicTrack < ActiveRecord::Base
   has_many :tracks, dependent: :destroy
   accepts_nested_attributes_for :tracks, :limit => 1, :reject_if => :all_blank, allow_destroy: true
 
-  has_attached_file :image, styles: { medium: "300x150#", thumb: "100X100#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
+  has_attached_file :image, styles: { medium: "300x150#", thumb: "100X100#", large: "560x290#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   validates :name, :author, :image, presence: { message: "Todos los campos son obligatorios"}
