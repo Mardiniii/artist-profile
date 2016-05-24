a269648221dd495e020d400c5c359eb93ec67072
mardiniii <sezama03@hotmail.com>
2016-05-24 16:56:50 -0500

    Add image association to background image with paperclip

--- app/models/background_image.rb
+++ app/models/background_image.rb
@@ -2,9 +2,13 @@
 #
 # Table name: background_images
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class BackgroundImage < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524214949) do
+ActiveRecord::Schema.define(version: 20160524215208) do
 
   create_table "background_images", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "blog_entries", force: :cascade do |t|
