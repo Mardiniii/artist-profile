1f39a93d816ab7683a10a7d1aa06c9e1d8e9541b
mardiniii <sezama03@hotmail.com>
2016-05-24 20:33:32 -0500

    Add gallery and image model in order to create albumns

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,9 @@
 # == Schema Information
 #
-# Table name: music_tracks
+# Table name: images
 #
 #  id                 :integer          not null, primary key
-#  name               :string
-#  author             :string
+#  gallery_id         :integer
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  image_file_name    :string
@@ -13,14 +12,9 @@
 #  image_updated_at   :datetime
 #
 
-class MusicTrack < ActiveRecord::Base
-  has_many :tracks, dependent: :destroy
-  accepts_nested_attributes_for :tracks, :limit => 50, :reject_if => :all_blank, allow_destroy: true
+class Image < ActiveRecord::Base
+  belongs_to :gallery
 
   has_attached_file :image, styles: { medium: "300x150#", thumb: "100X100#", large: "560x290#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-  validates :name, :author, :image, presence: { message: "Todos los campos son obligatorios"}
-
-  validates_associated :tracks
 end
---
+++ test/models/gallery_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: galleries
+#
+#  id                  :integer          not null, primary key
+#  title               :string
+#  description         :text
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+
+require 'test_helper'
+
+class GalleryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
