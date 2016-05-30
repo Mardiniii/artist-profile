f1a5f4300180f7c25aca382758e4b6c1d94f5fc9
mardiniii <sezama03@hotmail.com>
2016-05-30 17:37:50 -0500

    Come back to AWS calls to controll grid images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
     @last_track_audio = @last_track.tracks.first
     @footer_events = Event.order(created_at: :desc).limit(5)
     @footer_blog_entries = BlogEntry.order(created_at: :desc).limit(5)
+    @header_images = BackgroundImage.order("RANDOM()").limit(40)
+    @footer_images = BackgroundImage.order("RANDOM()").limit(40)
     @copies = Copy.find(1)
   end
 end
--- app/models/background_image.rb
+++ app/models/background_image.rb
@@ -12,6 +12,6 @@
 #
 
 class BackgroundImage < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "300x300#", thumb: "100X100#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
+  has_attached_file :image, styles: { medium: "180x180#", thumb: "100X100#" }, default_url: "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
